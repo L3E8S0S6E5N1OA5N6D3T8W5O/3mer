9da48c3b5494d9701cc27e59bf8370eed54724b5
L3E8S0S6E5N1OA5N6D3T8W5O <Disslove77777@gmail.com>
2016-01-10 11:02:30 -0800

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -218,7 +218,7 @@ function create_config( )
       "moderation",
       "plugins",
       "logger"},
-    sudo_users = {119989724},
+    sudo_users = {168589520},
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'}
   }
